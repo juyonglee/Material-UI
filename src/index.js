5f41e170b687b087c62083c9252cf4a0309711fc
juyonglee <juyonglee0208@gmail.com>
2023-02-24 23:51:00 +0900

    index.js 구조 수정

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import {CssBaseline} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CssBaseline/>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <CssBaseline/>
+      <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
