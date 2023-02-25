80d8314e9d99d25636bc08162e94524e5dab7a94
juyonglee <juyonglee0208@gmail.com>
2023-02-25 18:21:38 +0900

    index.js 구조 변경

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {CssBaseline} from "@mui/material";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <CssBaseline/>
       <App/>
     </React.StrictMode>
 );
