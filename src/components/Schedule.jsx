80e7eecc2876c9e37c522e0da17b901e23eca6d3
juyonglee <juyonglee0208@gmail.com>
2023-03-01 23:50:37 +0900

    Todo Content Area 작성

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,7 +1,21 @@
-import {Card, CardHeader, Container} from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Container, Divider,
+  List,
+  ListItem, Typography
+} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import {useState} from "react";
 
 export default function Schedule() {
+
+  const [state, setState] = useState({
+    todoList: []
+  })
+
   const date = new Date();
   return (
       <Container maxWidth="false">
@@ -15,6 +29,25 @@ export default function Schedule() {
                     subheader={date.toLocaleDateString()}
                     subheaderTypographyProps={{variant: "subtitle2"}}
                 />
+                <Divider variant={"middle"}/>
+                <CardContent sx={{minHeight: 500}}>
+                  {
+                    (state.todoList.length === 0) &&
+                        <Box sx={{minHeight: 500}} display="flex" justifyContent={"center"} alignItems={"center"}>
+                          <Typography variant={"body1"} fontWeight="400">오늘 할일을 추가해주세요.</Typography>
+                        </Box>
+                  }
+                  {
+                    state.todoList.map((todo, index) => {
+                      return (
+                          <List>
+                            <ListItem>
+                            </ListItem>
+                          </List>
+                      )
+                    })
+                  }
+                </CardContent>
               </Card>
             </Grid>
           </Grid>
