b7c6588a8eec4c114afdb9c02af0cb6c39a7da78
juyonglee <juyonglee0208@gmail.com>
2023-03-03 03:16:27 +0900

    Todo List 구현을 위한 Sample Data 추가

--- src/sample/todoList.js
+++ src/sample/todoList.js
@@ -1,18 +1,50 @@
 export const todoList = [
   {
     id: 0,
-    todo: "MUI 사용하기"
+    todo: "MUI 사용하기",
+    complete: false,
+    completeTime: null
   },
   {
     id: 1,
-    todo: "React Study"
+    todo: "React Study",
+    complete: false,
+    completeTime: null
   },
   {
     id: 2,
-    todo: "JavaScript Study"
+    todo: "JavaScript Study",
+    complete: false,
+    completeTime: null
   },
   {
     id: 3,
-    todo: "Algorithm Study"
+    todo: "Algorithm Study",
+    complete: false,
+    completeTime: null
+  },
+  {
+    id: 4,
+    todo: "맛있는 저녁 먹으러가기 😀",
+    complete: false,
+    completeTime: null
+  },
+  {
+    id: 5,
+    todo: "Java Desing Pattern 정리 📗",
+    complete: false,
+    completeTime: null
+  },
+  {
+    id: 6,
+    todo: "Coding Test 준비 🧑🏻‍💻",
+    complete: false,
+    completeTime: null
+  },
+  {
+    id: 7,
+    todo: "TLS 구현을 위한 암호학 Study 🤔",
+    complete: false,
+    completeTime: null
   }
 ]
